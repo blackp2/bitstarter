ebb9a8bde9b23868111ed45c990e5157976d05cc
blackp2 <black.philip@gmail.com>
2013-07-04 21:22:30 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var output = new Buffer("");
+output = fs.readFileSync(index.html);
+
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(output.toString());
 });
 
 var port = process.env.PORT || 5000;
